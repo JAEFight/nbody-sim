34c9d174d436ffa0fa5b64b8cbafd0404e136a27
JAEFight <hertrichfinn@gmail.de>
2025-11-09 21:54:37 +0100

    Neues

--- gravity.py
+++ gravity.py
@@ -2,16 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-dt = 0.15 # Zeitschritte
+dt = 0.03 # Zeitschritte
 n_balls = 3 # Anzahl der kugeln
 
 # Startposition und Geschw.
 positions = np.random.uniform(1.1,9, (n_balls, 2)) # (a,b,(shape)) -> n_balls = Anzahl der Zeilen
-velocities = np.random.uniform(-0.5, .5, (n_balls, 2))
-radii = np.random.uniform(0.05, 0.15, n_balls)
-sizes = (radii * 100)**2
-print(radii)
-print(sizes)
+print(positions)
+velocities = np.array([
+    [0.5, 2.0],
+    [1.3, 1.0],
+    [-1.0, 0.45]
+])
 """
 positions = np.array([
     [0.0, 0.0]
@@ -33,25 +34,24 @@ fig, ax = plt.subplots()
 ax.set_xlim(0,10)
 ax.set_ylim(0,10)
 ax.set_aspect("equal")
-#colors = np.random.rand(n_balls, 3)
-colors = np.array([
-    [1.0, 0, 0],
-    [0, 1.0, 0],
-    [0, 0, 1.0]
-])
-scat = ax.scatter(positions[:,0], positions[:,1], s= sizes, c=colors)  # Alle x und y positionen
+scat = ax.scatter(positions[:,0], positions[:,1], s= 100)  # Alle x und y positionen
 
 def update(frame):
     global positions,velocities
     
     # Bewegung
-    positions += velocities * dt
+    positions += (velocities-[0,0.981]) * dt
     # Beschleunigung -> positions += (velocities + i)*dt
 
     # Reflexion an Wänden
     for i in range(n_balls):
         for dim in [0,1]:
-            if positions[i, dim] <= radii[i] or positions[i,dim] >= 10-radii[i]:  # positions[i, dim] === Kugel i entweder dim= 0 für x-koord oder dim = 1 für y-koord
+            if positions[i, dim] < 0.5:  # positions[i, dim] === Kugel i entweder dim= 0 für x-koord oder dim = 1 für y-koord
+                positions[i,dim] = 0.5
+                velocities[i, dim] *= -1 # Richtung x und y gespiegelt
+                print(velocities[i, dim])
+            elif positions[i,dim] > 9.5:
+                positions[i,dim] = 9.5
                 velocities[i, dim] *= -1 # Richtung x und y gespiegelt
     scat.set_offsets(positions)
     return scat,
